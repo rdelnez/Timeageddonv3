271b5dc31f736832cdebf1b29a5eaf3150fbc894
rdelnez <rdelnez@gmail.com>
2016-12-08 16:22:03 +1100

    Added Assets for Will's Game
    Added most of the assets

---
+++ Assets/Scripts/TeslaLevelScripts/Tesla_GM.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Tesla_GM : MonoBehaviour {
+
+	public GameObject menuPanel;
+	public GameObject gamePanel;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void ReturnToGame(){
+		menuPanel.SetActive (false);
+		gamePanel.SetActive (true);
+	}
+
+	public void ReturnToMenu(){
+		menuPanel.SetActive (true);
+		gamePanel.SetActive (false);
+	}
+
+}
