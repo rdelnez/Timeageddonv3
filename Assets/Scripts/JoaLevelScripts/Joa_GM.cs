4d61198266590e64bc4d2853c45f84324ad5bb76
rdelnez <rdelnez@gmail.com>
2016-12-08 12:05:00 +1100

    Added Joa_GM
    Added the Game Manager Script for Joa

---
+++ Assets/Scripts/JoaLevelScripts/Joa_GM.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Joa_GM : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
