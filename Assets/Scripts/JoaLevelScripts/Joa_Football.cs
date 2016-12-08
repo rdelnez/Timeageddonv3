97d5c00bd0c9fd8adb97d3e0f1bea0843b360821
Kolev88 <Kolevski.daniel@gmail.com>
2016-12-08 14:01:51 +1100

    Added Joa_Football to football.prefab
    Added a script that will be used to move soccer ball on JOA level.

---
+++ Assets/Scripts/JoaLevelScripts/Joa_Football.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Joa_Football : MonoBehaviour {
+
+	public float speed;
+
+	private Rigidbody rb;
+
+	// Use this for initialization
+	void Start () {
+	
+		rb = GetComponent<Rigidbody> ();
+
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+	
+		transform.position = new Vector3 (0, 0, 40);
+
+	}
+}
--- Assets/Scripts/JoaLevelScripts/Joa_GM.cs
+++ Assets/Scripts/JoaLevelScripts/Joa_GM.cs
@@ -3,13 +3,26 @@ using System.Collections;
 
 public class Joa_GM : MonoBehaviour {
 
+	public Vector3 origPos = new Vector3 (1, 1, -11.73f);
+	public Object football;
+	public GameObject footballInstantiated;
+
+
 	// Use this for initialization
 	void Start () {
-	
+
+		football = Resources.Load("JoaLevel/Prefabs/football");
+
 	}
 	
 	// Update is called once per frame
 	void Update () {
 	
+		if (Input.GetButtonDown ("Fire1")) {
+
+			footballInstantiated = Instantiate(football, origPos, Quaternion.identity) as GameObject;
+
+		}
+
 	}
 }
