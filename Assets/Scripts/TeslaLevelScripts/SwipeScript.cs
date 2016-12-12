8aee59faefc881e5180f5cbd1e6283069ff2a193
rdelnez <rdelnez@gmail.com>
2016-12-12 16:34:13 +1100

    Done Bouncy Ground and Swiping using Mouse Click
    Changed Touch to MouseClick

--- Assets/Scripts/TeslaLevelScripts/SwipeScript.cs
+++ Assets/Scripts/TeslaLevelScripts/SwipeScript.cs
@@ -10,18 +10,28 @@ public class SwipeScript : MonoBehaviour {
 	public GameObject ballObject;
 
 
-	private float startTime  = 0.0f;
-	private Vector2 startPos = Vector2.zero;
-	private bool isSwiping = false;
+	public float startTime  = 0.0f;
+	public Vector2 startPos = Vector2.zero;
+	public Vector2 endPos = Vector2.zero;
+	public bool isSwiping = false;
+	public float swipeTime = 0.0f;
+	public float swipeDist = 0.0f;
 	private float minSwipeDist  = 20.0f;
 	private float maxSwipeTime = 1.0f;
+
+	[SerializeField]
 	private float xForceValue;
+	[SerializeField]
 	private float yForceValue;
+
+	private float maxYForce = 500.0f;
+	private float maxSwipeSpeed = 0.3f;
+
 	
 	
 	// Update is called once per frame
 	void Update () {
-		
+		/*--
 		if (Input.touchCount > 0){
 			
 			foreach (Touch touch in Input.touches)
@@ -73,7 +83,42 @@ public class SwipeScript : MonoBehaviour {
 
 			Invoke("ResetBall", 5.0f);
 		}
+
+		--*/
 		
+	}
+
+	public void AddForceToBall(){
+		if (isSwiping && swipeTime < maxSwipeTime && swipeDist > minSwipeDist){
+			
+			
+			//Start Force Calculation
+			xForceValue = endPos.x - startPos.x;
+			yForceValue = endPos.y - startPos.y;
+			
+			textSendObject.GetComponent<Text>().text = "swipe successful";
+			Debug.Log ("adding force");
+			
+			ballObject.GetComponent<Rigidbody>().isKinematic = false;
+			if(yForceValue > maxYForce){
+				yForceValue = maxYForce;
+			}
+
+			if(swipeTime < maxSwipeSpeed){
+				swipeTime = maxSwipeSpeed;
+			}
+			ballObject.GetComponent<Rigidbody>().AddForce(new Vector3(xForceValue,yForceValue,yForceValue)*(2/swipeTime));
+			ballObject.GetComponent<Rigidbody>().AddTorque(new Vector3(swipeDist*2.0f,0,0));
+			StartCoroutine(ScaleBall());
+
+			Invoke("ResetBall", 4.0f);
+			
+			
+			//End Force Calculation
+			
+			
+		}
+
 	}
 
 	public void SendMessage(){
@@ -84,7 +129,7 @@ public class SwipeScript : MonoBehaviour {
 	public void ResetBall(){
 
 		ballObject.GetComponent<Rigidbody>().isKinematic = true;
-		ballObject.transform.localPosition = new Vector3 (0, 1.19f, -2.74f);
+		ballObject.transform.localPosition = new Vector3 (0, -3.44f, -151.0f);
 		ballObject.transform.localEulerAngles = new Vector3 (90,60,0);
 		ballObject.transform.localScale = new Vector3 (1, 1, 1);
 	}
@@ -97,4 +142,5 @@ public class SwipeScript : MonoBehaviour {
 		}
 
 	}
+
 }
---
+++ Assets/Scripts/TeslaLevelScripts/Tesla_Ball.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Tesla_Ball : MonoBehaviour {
+	
+	public GameObject swipeScriptObject;
+	public SwipeScript swipeScript;
+	// Use this for initialization
+	void Start () {
+		
+		swipeScript = swipeScriptObject.GetComponent<SwipeScript> ();
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+		
+		
+	}
+	
+	void OnMouseOver(){
+		
+		Debug.Log ("Mouse is over the ball");
+	}
+	
+	void OnMouseDown(){
+
+
+		swipeScript.isSwiping = true;
+		swipeScript.startTime = Time.time;
+		swipeScript.startPos = Input.mousePosition;
+
+	}
+	
+	void OnMouseUp(){
+		swipeScript.endPos = Input.mousePosition;
+		Debug.Log ("Mouse is Up or Been depressed");
+
+		swipeScript.swipeTime = Time.time - swipeScript.startTime;
+		swipeScript.swipeDist = (swipeScript.endPos - swipeScript.startPos).magnitude;
+
+		swipeScript.AddForceToBall ();
+
+
+
+	}
+	
+	void OnMouseExit(){
+		
+		Debug.Log ("Mouse is out");
+		
+	}
+}
