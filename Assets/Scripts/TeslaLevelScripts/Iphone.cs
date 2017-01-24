fa4560ac6038036d2d0738085f57fd90ab459977
rdelnez <rdelnez@gmail.com>
2017-01-24 14:05:50 +1100

    Fixed some graphics
    Changed menu graphics

---
+++ Assets/Scripts/TeslaLevelScripts/Iphone.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Iphone : MonoBehaviour {
+
+	public Object particle;
+	public GameObject particlePrefabs;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.localPosition += new Vector3 (0.05f, 0, 0); 
+		transform.localEulerAngles += new Vector3 (0, 10.05f, 0);
+		particlePrefabs = Instantiate (particle, this.transform.localPosition, Quaternion.identity) as GameObject;
+	}
+
+
+}
